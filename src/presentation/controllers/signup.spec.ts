55266147d846e83ffa173cb08444319e0941d67b
Felipe Nascimento <felipe.rj@hotmail.com.br>
2021-04-03 19:31:42 -0300

    test: ensure SignUpController return an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,17 +1,18 @@
-import { SignUpController } from './signup';
+import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController();
+    const sut = new SignUpController()
 
     const httpRequest = {
       // name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password',
-      passwordConfirmation: 'any_password',
-    };
+      passwordConfirmation: 'any_password'
+    }
 
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missign param: name'))
   })
-});
+})
