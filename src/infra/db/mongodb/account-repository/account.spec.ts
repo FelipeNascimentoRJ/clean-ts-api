6c6e549031c216c4539e86722c9de9484ebdd071
Felipe Nascimento <felipe.rj@hotmail.com.br>
2021-04-10 20:43:36 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,32 @@
+import { AccountMongoRepository } from './account'
+
+import { MongoHelper } from '../helpers/mongodb-helper'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+
+    const accountData: AddAccountModel = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(account.name)
+    expect(account.email).toBe(account.email)
+    expect(account.password).toBe(account.password)
+  })
+})
